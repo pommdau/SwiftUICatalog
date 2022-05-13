bb684a52f0fe5571ea6d5bad71187d44cf739ce1
IKEH1024 <pondau1024@gmail.com>
2022-05-13 16:56:39 +0900

    keep reading at chapter7

--- CombineIntroduce/CombineIntroduce.playground/Pages/Subjects.xcplaygroundpage/Contents.swift
+++ CombineIntroduce/CombineIntroduce.playground/Pages/Subjects.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ import SwiftUI
 
 do {
     class Logger {
-        var tappedEvent = PassthroughSubject<String, Never>()
+        var tappedEvent = PassthroughSubject<String, Never>()  // PassthroughSubject: sendメソッドを持つPublisher
     }
 
     let logger = Logger()
@@ -16,12 +16,14 @@ do {
     logger.tappedEvent.send("LoginButton")
     logger.tappedEvent.send("CameraButton")
     subscriber.cancel()
-    logger.tappedEvent.send("LogoutButton")
+    logger.tappedEvent.send("LogoutButton")  // 受信されない
 }
+
+print("*** 🐱 ***")
 
 do {
     class Logger {
-        var tappedEvent = CurrentValueSubject<String, Never>("")
+        var tappedEvent = CurrentValueSubject<String, Never>("")  // 送信する値をvalueプロパティに保持する。初期化のイベントも発行される.
     }
 
     let logger = Logger()
@@ -29,16 +31,17 @@ do {
         print("event Name: \(event)")
     })
     logger.tappedEvent.send("LoginButton")
-    logger.tappedEvent.value = "CameraButton"
-
+    logger.tappedEvent.value = "CameraButton"  // Valueプロパティの直接更新でも値を送信できる
+//    logger.tappedEvent.send(logger.tappedEvent.value)
     subscriber.cancel()
-
     logger.tappedEvent.value = "LogoutButton"
 }
 
+print("*** 🐶 ***")
+
 do {
     class Logger {
-        @Published var tappedEventName: String = ""
+        @Published var tappedEventName: String = ""  // CurrentValueSubjectと同じく初期化のイベントも発行される
     }
     let logger = Logger()
     let subscriber = logger.$tappedEventName.sink(receiveValue: { event in
